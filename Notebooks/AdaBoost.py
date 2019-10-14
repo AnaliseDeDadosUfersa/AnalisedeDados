b9d2a83be4588bb7395e875d23a2f732a117a736
ArthurOlive <arthuroliveira909.av@gmail.com>
2019-10-14 14:32:35 -0300

    delete

--- Notebooks/AdaBoost.py
+++
@@ -1,6 +0,0 @@
-from sklearn.ensemble import AdaBoostClassifier
-from CommonModel import CommonModel
-
-class AdaBoost (CommonModel):
-    def __init__(self, link, positionTarget, language = "en"):
-        super().__init__(link, positionTarget, AdaBoostClassifier(n_estimators=100, random_state=0), language) 
--- Notebooks/ModelMLP.py
+++
@@ -1,5 +0,0 @@
-from sklearn.neural_network import MLPClassifier as MLP
-from CommonModel import CommonModel
-class ModelMLP(CommonModel):
-    def __init__(self, link, position):
-        super().__init__(link, position, MLP())
